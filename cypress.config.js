4c5618d3c5f52af633851513bae4740612692f86
Sabrina Lewis <brinazlewis@gmail.com>
2022-12-15 10:56:52 -0700

    d

---
+++ cypress.config.js
@@ -0,0 +1,19 @@
+const { defineConfig } = require("cypress");
+
+module.exports = defineConfig({
+  e2e: {
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
+    baseUrl: 'https://ui-automation-camp.vercel.app/',
+    specPattern: 'cypress/test/*/.cy.{js,jsx,ts,tsx}',
+  
+    reporter: 'mochawesome',
+    reporterOptions: {
+        reportDir: 'cypress/reports',
+        overwrite: false,
+        html: false,
+        json: true
+      },
+  },
+});
